358de7c2760ffd119f4dc729c8cdf01454951886
David Abrahams <da.j.abrahams@gmail.com>
2024-10-09 18:27:05 -0700

    add lots of comments

--- optimizer.py
+++ optimizer.py
@@ -61,39 +61,40 @@ class AdamW(Optimizer):
                 # self.state is a defaultdict, where the keys are Parameters, and the values are dictionaries
                 # The first time we call `step`, the state dictionary will be empty, and we initialize parameters
                 # using the code below.
-                state: dict[str, Union[int, Tensor]] = self.state[p]
-                assert isinstance(state, dict)
-                if not state:
-                    state['step'] = 0
+                param_state: dict[str, Union[int, Tensor]] = self.state[p]
+                assert isinstance(param_state, dict)
+                if not param_state:
+                    param_state['step'] = 0
                     # Initialize first moment (mean) vector m_t
-                    state['exp_avg'] = torch.zeros_like(p.data)
+                    param_state['exp_avg'] = torch.zeros_like(p.data)
                     # Initialize second moment (variance) vector v_t
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
-
-                # Update step count
-                state['step'] += 1
+                    param_state['exp_avg_sq'] = torch.zeros_like(p.data)
+                param_state['step'] += 1
 
                 # Get first and second moment variables
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                exp_avg: Tensor = param_state['exp_avg']
+                exp_avg_sq: Tensor = param_state['exp_avg_sq']
 
-                # Update biased first moment estimate
+                # exp_avg = exp_avg * beta1 + grad * (1 - beta1)
                 exp_avg.mul_(group.betas[0]).add_(grad, alpha=(1 - group.betas[0]))
 
-                # Update biased second raw moment estimate
+                # addcmul multiplies together grad * grad
+                # exp_avg_sq = exp_avg_sq * beta2 + (grad^2) * (1 - beta2)
                 exp_avg_sq.mul_(group.betas[1]).addcmul_(grad, grad, value=(1 - group.betas[1]))
 
-                # Bias correction
+                # Bias correction. Momentum / Variance are initially biased toward 0, so we divide by
+                # (1 - beta^t). When t is small, this increases momentum/variance. When t is large,
+                # we are dividing by 1 (no-op)
+                bias_correction1 = 1
+                bias_correction2 = 1
                 if group.correct_bias:
-                    bias_correction1 = 1 - group.betas[0] ** state['step']
-                    bias_correction2 = 1 - group.betas[1] ** state['step']
-                    corrected_exp_avg = exp_avg / bias_correction1
-                    corrected_exp_avg_sq = exp_avg_sq / bias_correction2
-                else:
-                    corrected_exp_avg = exp_avg
-                    corrected_exp_avg_sq = exp_avg_sq
+                    bias_correction1 -= group.betas[0] ** param_state['step']
+                    bias_correction2 -= group.betas[1] ** param_state['step']
+                corrected_exp_avg = exp_avg / bias_correction1
+                corrected_exp_avg_sq = exp_avg_sq / bias_correction2
 
-                # Update parameter
                 denom = corrected_exp_avg_sq.sqrt().add_(group.eps)
+                # add the first moment divided by the sqrt of second moment, scaled by the LR
                 p.data.addcdiv_(corrected_exp_avg, denom, value=-group.lr)
 
                 # Apply weight decay
